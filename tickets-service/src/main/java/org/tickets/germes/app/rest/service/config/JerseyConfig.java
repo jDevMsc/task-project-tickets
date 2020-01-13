9a81a5167f00481571619dd11dd8e12a6ddfc303
Artem <unlimitet2011@yandex.ru>
2020-01-13 19:08:24 +0300

    [TicketsApp]  Swagger config

--- tickets-service/src/main/java/org/tickets/germes/app/rest/service/config/JerseyConfig.java
+++ tickets-service/src/main/java/org/tickets/germes/app/rest/service/config/JerseyConfig.java
@@ -1,5 +1,8 @@
 package org.tickets.germes.app.rest.service.config;
 
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import javax.ws.rs.ApplicationPath;
 
 import javax.ws.rs.core.Application;
@@ -14,6 +17,22 @@ public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         super(ComponentFeature.class);
         packages("org.tickets.germes.app.rest");
+
+        register(ApiListingResource.class);
+        register(SwaggerSerializers.class);
+    }
+
+    /**
+     * Config for Swagger
+     */
+    private void initBeanConfig() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setSchemes(new String[] { "http" });
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/");
+        beanConfig.setResourcePackage("org.tickets.germes.app.rest.service");
+        beanConfig.setScan(true);
     }
 }
 
